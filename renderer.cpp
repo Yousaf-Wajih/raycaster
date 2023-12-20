e24cb3568df9341228cdb305cf7273ca80454e32
Yousaf Wajih <yousafwajih@gmail.com>
2023-12-20 06:01:23 +0500

    Floor and sky drawing

--- renderer.cpp
+++ renderer.cpp
@@ -25,6 +25,14 @@ static Ray castRay(sf::Vector2f start, float angleInDegrees, const Map &map);
 
 void Renderer::draw3dView(sf::RenderTarget &target, const Player &player,
                           const Map &map) {
+  sf::RectangleShape rectangle(sf::Vector2f(SCREEN_W, SCREEN_H / 2.0f));
+  rectangle.setFillColor(sf::Color(100, 170, 250));
+  target.draw(rectangle);
+
+  rectangle.setPosition(0.0f, SCREEN_H / 2.0f);
+  rectangle.setFillColor(sf::Color(70, 70, 70));
+  target.draw(rectangle);
+
   float angle = player.angle - PLAYER_FOV / 2.0f;
   float angleIncrement = PLAYER_FOV / (float)NUM_RAYS;
   const float maxRenderDistance = MAX_RAYCAST_DEPTH * map.getCellSize();
